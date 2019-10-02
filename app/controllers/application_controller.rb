0ce7b417d4df83ee101d9a540eab26f5bf316bfc
EC2 Default User <ec2-user@ip-172-31-17-96.ap-southeast-1.compute.internal>
2019-10-02 08:18:26 +0000

    [Finish] 4-8

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  
+  before_action :authenticate_user!
+  
+  protect_from_forgery with: :exception
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
+  def after_sign_out_path_for(resource_or_scope)
+    root_path # ←redirect先にしたいpathを自分で書く
+  end
+
+end
